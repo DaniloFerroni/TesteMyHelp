8a54103e28f23ebe8e4a3d429780851ccc72a9d2
DaniloFerroni <danilo_ferroni09@hotmail.com>
2015-11-08 00:37:57 -0200

    Testes Homologacao commit Teste

--- TeesteWeb/Controllers/AccountController.cs
+++ TeesteWeb/Controllers/AccountController.cs
@@ -20,6 +20,7 @@ namespace TeesteWeb.Controllers
 
         public AccountController()
         {
+            
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
